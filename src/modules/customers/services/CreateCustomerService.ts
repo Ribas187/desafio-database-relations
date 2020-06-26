fbd8f5688d11ed9c91a11a16191fb3431d81153d
ribas187 <ribas@Guilhermes-MacBook-Pro.local>
2020-06-26 13:49:45 -0300

    finished

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkExists = await this.customersRepository.findByEmail(email);
+
+    if (checkExists) {
+      throw new AppError('Email already exists');
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,66 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer ID does not exist');
+    }
+
+    const productsList = await this.productsRepository.findAllById(products);
+
+    if (productsList.length !== products.length) {
+      throw new AppError('Some of the products in order do not exist');
+    }
+
+    const orderProducts = productsList.map(product => {
+      const orderProduct = products.find(prod => prod.id === product.id);
+
+      if (!orderProduct) {
+        throw new AppError('Product error');
+      } else if (orderProduct.quantity > product.quantity) {
+        throw new AppError('Requested quantity is not available');
+      }
+
+      return {
+        ...product,
+        product_id: product.id,
+        quantity: orderProduct.quantity,
+      };
+    });
+
+    const orderProductsQuantities = productsList.map(product => {
+      const orderProduct = products.find(prod => prod.id === product.id);
+
+      if (!orderProduct) {
+        throw new AppError('Product error');
+      }
+
+      return {
+        id: product.id,
+        quantity: product.quantity - orderProduct.quantity,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(orderProductsQuantities);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
